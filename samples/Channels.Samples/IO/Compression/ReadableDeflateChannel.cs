584f65689ee20c670350e294028228fccf122a44
David Fowler <davidfowl@gmail.com>
2016-08-15 09:39:32 -0700

    WIP

--- samples/Channels.Samples/IO/Compression/ReadableDeflateChannel.cs
+++ samples/Channels.Samples/IO/Compression/ReadableDeflateChannel.cs
@@ -2,62 +2,158 @@
 using System.Collections.Generic;
 using System.IO.Compression;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Channels.Samples.IO.Compression
 {
-    public class ReadableDeflateChannel : ReadableChannel
+    public static class ChannelFactoryExtensions
     {
-        private readonly Inflater _inflater;
-
-        public ReadableDeflateChannel(IReadableChannel inner, MemoryPool pool) : this(inner, ZLibNative.Deflate_DefaultWindowBits, pool)
+        public static IReadableChannel CreateDeflateDecompressChannel(this ChannelFactory factory, IReadableChannel channel)
         {
+            var inflater = new ReadableDeflateChannel(ZLibNative.Deflate_DefaultWindowBits);
+            return factory.MakeReadableChannel(channel, inflater.Execute);
         }
 
-        public ReadableDeflateChannel(IReadableChannel inner, int bits, MemoryPool pool) : base(pool)
+        public static IReadableChannel CreateDeflateCompressChannel(this ChannelFactory factory, IReadableChannel channel, CompressionLevel compressionLevel)
         {
-            _inflater = new Inflater(bits);
-
-            DoDeflate(inner);
+            var deflater = new WritableDeflateChannel(compressionLevel, ZLibNative.Deflate_DefaultWindowBits);
+            return factory.MakeReadableChannel(channel, deflater.Execute);
         }
 
-        private async void DoDeflate(IReadableChannel inner)
+        public static IReadableChannel CreateGZipDecompressChannel(this ChannelFactory factory, IReadableChannel channel)
         {
-            while (true)
+            var inflater = new ReadableDeflateChannel(ZLibNative.GZip_DefaultWindowBits);
+            return factory.MakeReadableChannel(channel, inflater.Execute);
+        }
+
+        public static IWritableChannel CreateGZipCompressChannel(this ChannelFactory factory, IWritableChannel channel, CompressionLevel compressionLevel)
+        {
+            var deflater = new WritableDeflateChannel(compressionLevel, ZLibNative.GZip_DefaultWindowBits);
+            return factory.MakeWriteableChannel(channel, deflater.Execute);
+        }
+
+        public static IReadableChannel CreateGZipCompressChannel(this ChannelFactory factory, IReadableChannel channel, CompressionLevel compressionLevel)
+        {
+            var deflater = new WritableDeflateChannel(compressionLevel, ZLibNative.GZip_DefaultWindowBits);
+            return factory.MakeReadableChannel(channel, deflater.Execute);
+        }
+
+        private class WritableDeflateChannel
+        {
+            private readonly Deflater _deflater;
+
+            public WritableDeflateChannel(CompressionLevel compressionLevel, int bits)
             {
-                await inner;
-
-                var readBuffer = inner.BeginRead();
-                var end = readBuffer;
-
-                BufferSpan span;
-                if (!end.TryGetBuffer(out span) && inner.Completion.IsCompleted)
-                {
-                    break;
-                }
-
-                var writerBuffer = _channel.BeginWrite(2048);
- 
-                _inflater.SetInput(span.BufferPtr, span.Length);
-
-                int written = _inflater.Inflate(writerBuffer.Memory.BufferPtr, writerBuffer.Memory.Length);
-
-                writerBuffer.UpdateWritten(written);
-
-                var consumed = span.Length - _inflater.AvailableInput;
-
-                // Move the read iterator
-                readBuffer.Seek(consumed);
-
-                inner.EndRead(readBuffer);
-
-                await _channel.EndWriteAsync(writerBuffer);
+                _deflater = new Deflater(compressionLevel, bits);
             }
 
-            inner.CompleteReading();
+            public async Task Execute(IReadableChannel input, IWritableChannel output)
+            {
+                while (true)
+                {
+                    await input;
 
-            _channel.CompleteWriting();
+                    var readBuffer = input.BeginRead();
+                    var end = readBuffer;
 
-            _inflater.Dispose();
+                    BufferSpan span;
+                    if (!end.TryGetBuffer(out span) && input.Completion.IsCompleted)
+                    {
+                        break;
+                    }
+
+                    var writerBuffer = output.BeginWrite(2048);
+
+                    _deflater.SetInput(span.BufferPtr, span.Length);
+
+                    while (!_deflater.NeedsInput())
+                    {
+                        int written = _deflater.ReadDeflateOutput(writerBuffer.Memory.BufferPtr, writerBuffer.Memory.Length);
+                        writerBuffer.UpdateWritten(written);
+                    }
+
+                    var consumed = span.Length - _deflater.AvailableInput;
+
+                    // Move the read iterator
+                    readBuffer.Seek(consumed);
+
+                    input.EndRead(readBuffer);
+
+                    await output.EndWriteAsync(writerBuffer);
+                }
+
+                bool finished;
+                do
+                {
+                    // Need to do more stuff here
+                    var writerBuffer = output.BeginWrite(2048);
+
+                    int compressedBytes;
+                    finished = _deflater.Finish(writerBuffer.Memory.BufferPtr, writerBuffer.Memory.Length, out compressedBytes);
+
+                    await output.EndWriteAsync(writerBuffer);
+                }
+                while (!finished);
+
+                input.CompleteReading();
+
+                output.CompleteWriting();
+
+                _deflater.Dispose();
+            }
+        }
+
+        private class ReadableDeflateChannel
+        {
+            private readonly Inflater _inflater;
+
+            public ReadableDeflateChannel(int bits)
+            {
+                _inflater = new Inflater(bits);
+            }
+
+            public async Task Execute(IReadableChannel input, IWritableChannel output)
+            {
+                while (true)
+                {
+                    await input;
+
+                    var readBuffer = input.BeginRead();
+                    var end = readBuffer;
+
+                    BufferSpan span;
+                    if (!end.TryGetBuffer(out span) && input.Completion.IsCompleted)
+                    {
+                        break;
+                    }
+
+                    var writerBuffer = output.BeginWrite(2048);
+
+                    if (span.Length > 0)
+                    {
+                        _inflater.SetInput(span.BufferPtr, span.Length);
+
+                        int written = _inflater.Inflate(writerBuffer.Memory.BufferPtr, writerBuffer.Memory.Length);
+
+                        writerBuffer.UpdateWritten(written);
+
+                        var consumed = span.Length - _inflater.AvailableInput;
+
+                        // Move the read iterator
+                        readBuffer.Seek(consumed);
+                    }
+
+                    input.EndRead(readBuffer);
+
+                    await output.EndWriteAsync(writerBuffer);
+                }
+
+                input.CompleteReading();
+
+                output.CompleteWriting();
+
+                _inflater.Dispose();
+            }
         }
     }
 }
--- samples/Channels.Samples/Program.cs
+++ samples/Channels.Samples/Program.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Channels.Samples.Http;
+using Channels.Samples.IO;
+using Channels.Samples.IO.Compression;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
@@ -18,20 +20,62 @@ namespace Channels.Samples
     {
         public static void Main(string[] args)
         {
+            // RunHttpServer();
+            RunCompressionSample();
+        }
+
+        private static void RunCompressionSample()
+        {
+            using (var pool = new MemoryPool())
+            {
+                var channelFactory = new ChannelFactory(pool);
+
+                var filePath = Path.GetFullPath("Program.cs");
+
+                //var fs = File.OpenRead(filePath);
+                //var compressed = new MemoryStream();
+                //var compressStream = new DeflateStream(compressed, CompressionMode.Compress);
+                //fs.CopyTo(compressStream);
+                //compressStream.Flush();
+                //compressed.Seek(0, SeekOrigin.Begin);
+                // var input = channelFactory.MakeReadableChannel(compressed);
+
+                var fs1 = File.OpenRead(filePath);
+                var input = channelFactory.MakeReadableChannel(fs1);
+                input = channelFactory.CreateDeflateCompressChannel(input, CompressionLevel.Optimal);
+
+                input = channelFactory.CreateDeflateDecompressChannel(input);
+
+                // Wrap the console in a writable channel
+                var output = channelFactory.MakeWriteableChannel(Console.OpenStandardOutput());
+
+                // Copy from the file channel to the console channel
+                input.CopyToAsync(output).GetAwaiter().GetResult();
+
+                input.CompleteReading();
+
+                output.CompleteWriting();
+
+                Console.ReadLine();
+            }
+        }
+
+        private static void RunHttpServer()
+        {
             var host = new WebHostBuilder()
-                            .ConfigureServices(services =>
-                            {
-                                // Use a custom server
-                                services.AddTransient<IServer, HttpServer>();
-                            })
-                            .Configure(app =>
-                            {
-                                app.Run(async context =>
-                                {
-                                    await context.Response.WriteAsync("Hello World");
-                                });
-                            })
-                            .Build();
+                                        .ConfigureServices(services =>
+                                        {
+                                            // Use a custom server
+                                            services.AddTransient<IServer, HttpServer>();
+                                        })
+                                        .Configure(app =>
+                                        {
+                                            app.Run(async context =>
+                                            {
+                                                await context.Response.WriteAsync("Hello World");
+                                            });
+                                        })
+                                        .Build();
             host.Run();
         }
     }
