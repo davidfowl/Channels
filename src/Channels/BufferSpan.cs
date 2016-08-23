f22caee942515b2227b5e2c0f3cc3e87ce794c30
Ben Adams <thundercat@illyriad.co.uk>
2016-08-23 19:00:22 +0100

    Slim BufferSpan

--- src/Channels/BufferSpan.cs
+++ src/Channels/BufferSpan.cs
@@ -9,23 +9,26 @@ namespace Channels
     // TODO: Replace this with Span<byte>
     public struct BufferSpan
     {
+        private readonly MemoryBlockSegment _segment;
+        private readonly int _length;
+        private readonly int _offset;
+
         internal BufferSpan(MemoryBlockSegment segment, int offset, int count)
         {
-            UserData = segment.Block.Slab.UserData;
-            BufferPtr = segment.Block.DataArrayPtr + offset;
-            Length = count;
-            Offset = offset;
-            Array = segment.Block.Array;
+            _segment = segment;
+            _length = count;
+            _offset = offset;
         }
 
-        public byte[] Array { get; }
+        public byte[] Array => _segment.Block.Array;
 
-        public object UserData { get; }
+        public object UserData => _segment.Block.Slab.UserData;
 
-        public IntPtr BufferPtr { get; }
+        public IntPtr BufferPtr => _segment.Block.DataArrayPtr + _offset;
 
-        public int Length { get; }
-        public int Offset { get; }
+        public int Length => _length;
+
+        public int Offset => _offset;
 
         public override string ToString()
         {
