14cc721bac1e24814d41e7f7529ff86d400e7317
David Fowler <davidfowl@gmail.com>
2016-09-01 14:45:21 -0700

    Move ChannelAwaitable to another file

---
+++ src/Channels/ChannelAwaitable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Channels
+{
+    public struct ChannelAwaitable : ICriticalNotifyCompletion
+    {
+        private readonly Channel _channel;
+
+        internal ChannelAwaitable(Channel channel)
+        {
+            _channel = channel;
+        }
+
+        public bool IsCompleted => _channel.IsCompleted;
+
+        public ReadableBuffer GetResult() => _channel.GetResult();
+
+        public ChannelAwaitable GetAwaiter() => this;
+
+        public void UnsafeOnCompleted(Action continuation) => _channel.UnsafeOnCompleted(continuation);
+
+        public void OnCompleted(Action continuation) => _channel.OnCompleted(continuation);
+    }
+}
--- src/Channels/IReadableChannel.cs
+++ src/Channels/IReadableChannel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Channels
@@ -11,26 +10,5 @@ namespace Channels
         Task Completion { get; }
 
         void CompleteReading(Exception exception = null);
-    }
-
-    public struct ChannelAwaitable : ICriticalNotifyCompletion
-    {
-        private readonly Channel _channel;
-
-        internal ChannelAwaitable(Channel channel)
-        {
-            _channel = channel;
-        }
-
-        public bool IsCompleted => _channel.IsCompleted;
-
-        public ReadableBuffer GetResult() => _channel.GetResult();
-
-        public ChannelAwaitable GetAwaiter() => this;
-
-        public void UnsafeOnCompleted(Action continuation) => _channel.UnsafeOnCompleted(continuation);
-
-        public void OnCompleted(Action continuation) => _channel.OnCompleted(continuation);
-
     }
 }
