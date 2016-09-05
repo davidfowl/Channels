e443f65f3a8785a20c08a949d41fdfeaac06fafd
David Fowler <davidfowl@gmail.com>
2016-09-04 21:15:01 -0700

    Run tests in debug so we can see potential block leaks

--- test/Channels.Tests/ChannelFacts.cs
+++ test/Channels.Tests/ChannelFacts.cs
@@ -9,18 +9,20 @@ namespace Channels.Tests
         [Fact]
         public async Task WritingDataMakesDataReadableViaChannel()
         {
-            var cf = new ChannelFactory();
-            var channel = cf.CreateChannel();
-            var bytes = Encoding.ASCII.GetBytes("Hello World");
+            using (var cf = new ChannelFactory())
+            {
+                var channel = cf.CreateChannel();
+                var bytes = Encoding.ASCII.GetBytes("Hello World");
 
-            await channel.WriteAsync(bytes, 0, bytes.Length);
-            var buffer = await channel.ReadAsync();
+                await channel.WriteAsync(bytes, 0, bytes.Length);
+                var buffer = await channel.ReadAsync();
 
-            Assert.Equal(11, buffer.Length);
-            Assert.True(buffer.IsSingleSpan);
-            var array = new byte[11];
-            buffer.FirstSpan.TryCopyTo(array);
-            Assert.Equal("Hello World", Encoding.ASCII.GetString(array));
+                Assert.Equal(11, buffer.Length);
+                Assert.True(buffer.IsSingleSpan);
+                var array = new byte[11];
+                buffer.FirstSpan.TryCopyTo(array);
+                Assert.Equal("Hello World", Encoding.ASCII.GetString(array));
+            }
         }
     }
 }
