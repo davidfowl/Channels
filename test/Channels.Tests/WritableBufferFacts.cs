997b55292430a7219a52f907deea9fde15a44db8
Marc Gravell <marc.gravell@gmail.com>
2016-09-08 15:40:36 +0100

    Added test

---
+++ test/Channels.Tests/WritableBufferFacts.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Channels.Tests
+{
+    public class WritableBufferFacts
+    {
+        [Fact]
+        public async Task CanWriteNothingToBuffer()
+        {
+            using (var memoryPool = new MemoryPool())
+            {
+                var channel = new Channel(memoryPool);
+                var buffer = channel.Alloc();
+                buffer.CommitBytes(0); // doing nothing, the hard way
+                await buffer.FlushAsync();
+            }
+        }
+    }
+}
