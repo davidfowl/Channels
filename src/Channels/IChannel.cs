046e3e4cadec854be559b372175f8203d5d28b2e
David Fowler <davidfowl@gmail.com>
2016-09-08 20:56:41 -0700

    Added new IChannel interface (tho nothing implements it yet) #58

---
+++ src/Channels/IChannel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Channels
+{
+    public interface IChannel : IDisposable
+    {
+        IReadableChannel Input { get; }
+        IWritableChannel Output { get; }
+    }
+}
