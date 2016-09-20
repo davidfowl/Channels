fdb74977c3e61c3f6e2676f9afc3094cbb83d663
David Fowler <davidfowl@gmail.com>
2016-09-19 22:30:28 -0700

    Added doc comments for Advance

--- src/Channels/IReadableChannel.cs
+++ src/Channels/IReadableChannel.cs
@@ -21,10 +21,14 @@ namespace Channels
         Task Reading { get; }
 
         /// <summary>
-        /// Moves the channel forward
+        /// Moves forward the channels read cursor to after the consumed data.
         /// </summary>
-        /// <param name="consumed"></param>
-        /// <param name="examined"></param>
+        /// <param name="consumed">Marks the extent of the data that has been succesfully proceesed.</param>
+        /// <param name="examined">Marks the extent of the data that has been read and examined.</param>
+        /// <remarks>
+        /// The memory for the consumed data will be released and no longer available. 
+        /// The examined data communicates to the channel when it should signal more data is available.
+        /// </remarks>
         void Advance(ReadCursor consumed, ReadCursor examined);
 
         /// <summary>
