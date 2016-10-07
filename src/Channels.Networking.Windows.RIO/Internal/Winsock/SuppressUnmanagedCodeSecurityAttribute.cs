fba0e42b24d72890e96df157807f75d9f887349e
Ben Adams <thundercat@illyriad.co.uk>
2016-10-07 05:01:16 +0100

    Update RIO licence

--- src/Channels.Networking.Windows.RIO/Internal/Winsock/SuppressUnmanagedCodeSecurityAttribute.cs
+++ src/Channels.Networking.Windows.RIO/Internal/Winsock/SuppressUnmanagedCodeSecurityAttribute.cs
@@ -1,5 +1,7 @@
-﻿
-#if !NET451 
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#if !NET451
 namespace System.Security
 {
     public class SuppressUnmanagedCodeSecurityAttribute : Attribute
