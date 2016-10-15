52d4775987ae2f236c7d2c02969542a2d09762c6
Ben Adams <thundercat@illyriad.co.uk>
2016-10-15 07:24:47 +0100

    Fix multiply

--- src/Channels/CommonVectors.cs
+++ src/Channels/CommonVectors.cs
@@ -11,7 +11,7 @@ namespace Channels
         {
             // Vector<byte> .ctor is a bit fussy to get working; however this always seems to work
             // https://github.com/dotnet/coreclr/issues/7459#issuecomment-253965670
-            return Vector.AsVectorByte(new Vector<ulong>(vectorByte * 0x0001000100010001ul));
+            return Vector.AsVectorByte(new Vector<ulong>(vectorByte * 0x0101010101010101ul));
         }
     }
 }
