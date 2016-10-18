823ad274f1c3c29bbe54cdc8abd9c40625e9107f
Drawaes <seawardtim@gmail.com>
2016-10-18 23:58:20 +0100

    Update to test Unix compatibility

--- src/Channels.Networking.TLS/Internal/OpenSsl/InteropCrypto.cs
+++ src/Channels.Networking.TLS/Internal/OpenSsl/InteropCrypto.cs
@@ -10,19 +10,85 @@ namespace Channels.Networking.TLS.Internal.OpenSsl
     {
         public const string CryptoDll = "libeay32.dll";
 
-        [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void OPENSSL_add_all_algorithms_noconf();
-        [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void ERR_load_crypto_strings();
-        [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void CRYPTO_free(void* pointer);
-
-        [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
-        public extern static int CRYPTO_num_locks();
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void locking_function(LockState mode, int threadNumber, byte* file, int line);
-        [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
-        public extern static void CRYPTO_set_locking_callback(locking_function lockingFunction);
+
+        private static class WindowsLib
+        {
+            public const string CryptoDll = "libeay32.dll";
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void CRYPTO_set_locking_callback(locking_function lockingFunction);
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void OPENSSL_add_all_algorithms_noconf();
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void ERR_load_crypto_strings();
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void CRYPTO_free(void* pointer);
+
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static int CRYPTO_num_locks();
+        }
+
+        private static class UnixLib
+        {
+            public const string CryptoDll = "libcrypto.so";
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void CRYPTO_set_locking_callback(locking_function lockingFunction);
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void OPENSSL_add_all_algorithms_noconf();
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void ERR_load_crypto_strings();
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static void CRYPTO_free(void* pointer);
+
+            [DllImport(CryptoDll, CallingConvention = CallingConvention.Cdecl)]
+            public extern static int CRYPTO_num_locks();
+        }
+
+        public static void CRYPTO_set_locking_callback(locking_function lockingFunction)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                WindowsLib.CRYPTO_set_locking_callback(lockingFunction);
+            }
+            else
+            {
+                UnixLib.CRYPTO_set_locking_callback(lockingFunction);
+            }
+        }
+        public static void OPENSSL_add_all_algorithms_noconf()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                WindowsLib.OPENSSL_add_all_algorithms_noconf();
+            }
+            else
+            {
+                UnixLib.OPENSSL_add_all_algorithms_noconf();
+            }
+        }
+        public static void ERR_load_crypto_strings()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                WindowsLib.ERR_load_crypto_strings();
+            }
+            else
+            {
+                UnixLib.ERR_load_crypto_strings();
+            }
+        }
+        public static void CRYPTO_free(void* pointer)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                WindowsLib.CRYPTO_free(pointer);
+            }
+            else
+            {
+                UnixLib.CRYPTO_free(pointer);
+            }
+        }
 
         [Flags]
         internal enum LockState
